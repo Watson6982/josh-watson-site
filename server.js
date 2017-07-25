093c239897f7e3caa89a18624e163443077a6a66
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2017-07-25 16:12:45 +0100

    adding basic html templating

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ app.use(express.static('public'))
 app.use(helmet()); // security stuffs
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use((req,res,next) => {
+	var log = {
+		body: req.body,
+		params: req.params,
+		queryStr: req.query,
+	};
+	console.log(JSON.stringify(log, null, 2));
+	next();
+}); // log specific request objects for clarity when testing
 
 // set up separate routes files for easier management
 app.use('/', routes);
