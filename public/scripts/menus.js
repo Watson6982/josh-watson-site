8fc3ce88c99a5375eef895139ace28b41223b79f
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2017-07-31 15:31:50 +0100

    ameding navbar width

--- public/scripts/menus.js
+++ public/scripts/menus.js
@@ -1,6 +1,6 @@
 
 function openNav() {
-    document.getElementById("myNav").style.width = "25%";
+    document.getElementById("myNav").style.width = "50%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
