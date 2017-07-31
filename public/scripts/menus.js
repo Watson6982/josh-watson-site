b85ba260817adf3768dd8b857e32f68584c68783
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2017-07-31 15:28:42 +0100

    adding sidebar navigation

---
+++ public/scripts/menus.js
@@ -0,0 +1,19 @@
+
+function openNav() {
+    document.getElementById("myNav").style.width = "25%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+/* Open when someone clicks on the span element */
+function openProfile() {
+    document.getElementById("myProfile").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeProfile() {
+    document.getElementById("myProfile").style.width = "0%";
+}
