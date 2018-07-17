b3dcc21ebcaad35af5d5e46747ea9d969b86c677
Joshua Watson <watson6982@hotmail.co.uk>
2018-07-18 00:05:49 +0100

    Add facebook image requesting

--- routes/site.js
+++ routes/site.js
@@ -1,9 +1,18 @@
+var request = require('request');
 var express = require('express');
 var router = express.Router();
 
 // 
 router.get('/', (req, res) => {
-    res.render('index');
+    var url = 'https://graph.facebook.com/100001719130569?fields=picture.width(720).height(720)';
+    var facebookprofile = request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+        var JSONdata = JSON.parse(body);
+        var imagelink = JSONdata.picture.data.url;
+        console.log(imagelink);
+        res.render('index', {profileURL: imagelink});
+        }
+    });
 });
 
 module.exports = router;
