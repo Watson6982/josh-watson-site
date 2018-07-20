6109f29418af24bc4f6696d1e9b35b88399a9497
Joshua Watson <watson6982@hotmail.co.uk>
2018-07-20 08:52:34 +0100

    add javascript opening for learning goals

---
+++ public/scripts/button.js
@@ -0,0 +1,19 @@
+function openJSGoals() {
+    document.getElementById("learningJS").style.display = "block";
+}
+
+function openDBGoals() {
+    document.getElementById("learningDB").style.display = "block";
+}
+
+function openHTTPGoals() {
+    document.getElementById("learningHTTP").style.display = "block";
+}
+
+function openWebGoals() {
+    document.getElementById("learningWeb").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("learningList").style.display = "none";
+}
